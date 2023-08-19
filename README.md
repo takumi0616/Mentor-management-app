fb2cd7ba2055560f84f36fde451fb6ee57531f5a
takumi0616 <s233319@stn.nagaokaut.ac.jp>
2023-08-20 03:02:41 +0900

    8/20_3:00

--- README.md
+++ README.md
@@ -6,12 +6,17 @@ application up and running.
 Things you may want to cover:
 
 * Ruby version
-3.0.6
+- 3.0.6
 * System dependencies
+- MySQL 8.0
 
 * Configuration
+- '.env'
 
 * Database creation
+docker-compose run api rails db:create
+docker-compose run api rails db:migrate
+docker compose run --rm api bundle exec rails db:seed
 
 * Database initialization
 
--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -42,7 +42,7 @@ ActiveRecord::Schema.define(version: 2023_08_18_153244) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "role", null: false
-    t.boolean "admin", default: false, null: false
+    t.boolean "admin", default: false
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
